f85a92e2ffe458f036f353416c194eef60abd173
alexandr.fasii <alexandr.fasii@reigncode.dev>
2022-05-24 15:54:42 +0300

    refactor

--- src/main/java/com/example/universetechapidemoapp/UniverseTechApiDemoAppApplication.java
+++ src/main/java/com/example/universetechapidemoapp/UniverseTechApiDemoAppApplication.java
@@ -1,12 +1,15 @@
 package com.example.universetechapidemoapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class UniverseTechApiDemoAppApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(UniverseTechApiDemoAppApplication.class, args);
+    log.info("Database url : http://localhost:8080/h2");
   }
 }
