6e034893b9f1c17e8edce65d55d56c5a78b615a9
alexandr.fasii <alexandr.fasii@reigncode.dev>
2022-06-03 17:08:29 +0300

    change port

--- src/main/java/com/example/universetechapidemoapp/UniverseTechApiDemoApplication.java
+++ src/main/java/com/example/universetechapidemoapp/UniverseTechApiDemoApplication.java
@@ -10,6 +10,10 @@ public class UniverseTechApiDemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(UniverseTechApiDemoApplication.class, args);
-    log.info("Database url : http://localhost:8080/h2");
+    log.info("Database url : http://localhost:8081/h2");
   }
 }
+
+// 1. Manual testing (Rest Api - UniverseTechApiDemoApplication) :
+//    - Написание Тест кейсов и их исполнение (если есть баги - репортим их)
+//    - Проверка базы данных
